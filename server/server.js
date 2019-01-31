027d8a744a37c9759322c4e58469e94a82f57381
Antarpreet Singh <antarpreet13@gmail.com>
2019-01-31 12:42:30 -0800

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,17 +14,35 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    })
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('disconnect', () => {
         console.log('Client Disconnected');
     });
 
     socket.on('createMessage', (msg) => {
         console.log('Create message', msg);
+        
         io.emit('newMessage', {
             from: msg.from,
             text: msg.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage', {
+        //     from: msg.from,
+        //     text: msg.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 });
 
