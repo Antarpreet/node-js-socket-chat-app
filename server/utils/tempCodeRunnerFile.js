b7a918e7532d6b34b56c50beb31797b54be03830
Antarpreet Singh <antarpreet13@gmail.com>
2019-01-31 18:25:46 -0800

    Add users class and updateUserList event

--- server/utils/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-describe('generateMessage', () => {
-    it('should generate the correct message object', () => {
-        var response = generateMessage('user', 'Find me');
-
-        expect(response.from).toBe('user');
-        expect(response.text).toBe('Find me');
-        expect(response.createdAt).toBeA('number');
-    });
-});
